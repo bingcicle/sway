c362f7703fd984f12eb3e8b01076860f37f38ab1
Andrew Cann <shum@canndrew.org>
2022-03-08 13:39:49 +0800

    remove pest::Span from Span

--- sway-types/src/span.rs
+++ sway-types/src/span.rs
@@ -1,10 +1,14 @@
-use std::{path::PathBuf, sync::Arc, borrow::Cow};
+use std::{path::PathBuf, sync::Arc, borrow::Cow, cmp};
 
 /// Represents a span of the source code in a specific file.
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct Span {
-    ///  A [pest::Span] returned directly from the generated parser.
-    span: pest::Span,
+    // The original source code.
+    src: Arc<str>,
+    // The byte position in the string of the start of the span.
+    start: usize,
+    // The byte position in the string of the end of the span.
+    end: usize,
     // A reference counted pointer to the file from which this span originated.
     path: Option<Arc<PathBuf>>,
 }
@@ -12,7 +16,9 @@ pub struct Span {
 impl Span {
     pub fn from_pest(pest_span: pest::Span, path: Option<Arc<PathBuf>>) -> Span {
         Span {
-            span: pest_span,
+            src: pest_span.input().clone(),
+            start: pest_span.start(),
+            end: pest_span.end(),
             path,
         }
     }
@@ -27,10 +33,10 @@ impl Span {
     */
 
     pub fn new(src: Arc<str>, start: usize, end: usize, path: Option<Arc<PathBuf>>) -> Option<Span> {
-        Some(Span {
-            span: pest::Span::new(src, start, end)?,
-            path,
-        })
+        if src.get(start..end).is_none() {
+            return None;
+        }
+        Some(Span { src, start, end, path })
     }
 
     /*
@@ -43,15 +49,15 @@ impl Span {
     */
 
     pub fn src(&self) -> &Arc<str> {
-        self.span.input()
+        &self.src
     }
 
     pub fn start(&self) -> usize {
-        self.span.start()
+        self.start
     }
 
     pub fn end(&self) -> usize {
-        self.span.end()
+        self.end
     }
 
     pub fn path(&self) -> Option<&Arc<PathBuf>> {
@@ -63,27 +69,29 @@ impl Span {
     }
 
     pub fn start_pos(&self) -> pest::Position {
-        self.span.start_pos()
+        pest::Position::new(self.src.clone(), self.start).unwrap()
     }
 
     pub fn end_pos(&self) -> pest::Position {
-        self.span.end_pos()
+        pest::Position::new(self.src.clone(), self.end).unwrap()
     }
 
     pub fn split(&self) -> (pest::Position, pest::Position) {
-        self.span.clone().split()
+        let start = self.start_pos();
+        let end = self.end_pos();
+        (start, end)
     }
 
     pub fn str(self) -> String {
-        self.span.as_str().to_string()
+        self.as_str().to_owned()
     }
 
     pub fn as_str(&self) -> &str {
-        self.span.as_str()
+        &self.src[self.start..self.end]
     }
 
     pub fn input(&self) -> &str {
-        self.span.input()
+        &self.src
     }
 
     /*
@@ -98,14 +106,10 @@ impl Span {
     pub fn trim(self) -> Span {
         let start_delta = self.as_str().len() - self.as_str().trim_start().len();
         let end_delta = self.as_str().len() - self.as_str().trim_end().len();
-        let span = pest::Span::new(
-            self.span.input().clone(),
-            self.span.start() + start_delta,
-            self.span.end() - end_delta,
-        )
-        .unwrap();
         Span {
-            span,
+            src: self.src,
+            start: self.start + start_delta,
+            end: self.end - end_delta,
             path: self.path,
         }
     }
@@ -114,25 +118,20 @@ impl Span {
 /// This panics if the spans are not from the same file. This should
 /// only be used on spans that are actually next to each other.
 pub fn join_spans(s1: Span, s2: Span) -> Span {
+    // FIXME(canndrew): This is horrifying. Where did it come from and why is it needed?
     if s1.as_str() == "core" {
         return s2;
     }
+
     assert!(
-        s1.input() == s2.input() && s1.path == s2.path,
+        Arc::ptr_eq(&s1.src, &s2.src) && s1.path == s2.path,
         "Spans from different files cannot be joined.",
     );
 
-    let s1_positions = s1.split();
-    let s2_positions = s2.split();
-    if s1_positions.0 < s2_positions.1 {
-        Span {
-            span: s1_positions.0.span(&s2_positions.1),
-            path: s1.path,
-        }
-    } else {
-        Span {
-            span: s2_positions.0.span(&s1_positions.1),
-            path: s1.path,
-        }
+    Span {
+        src: s1.src,
+        start: cmp::min(s1.start, s2.start),
+        end: cmp::max(s1.end, s2.end),
+        path: s1.path,
     }
 }
